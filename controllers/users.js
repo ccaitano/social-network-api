7fcacfc08379c79a1aeeb97e341531023da5bbd7
ccaitano <cheryl.caitano@gmail.com>
2022-10-08 11:54:09 -1000

    Outline user controller file

---
+++ controllers/users.js
@@ -0,0 +1,34 @@
+const { User } = require('../models');
+
+const userController = {
+    // Get all users
+    getAllUsers(req, res) {
+
+    },
+    // Get a singlie user by ID
+    getUserById({ params }, res) {
+
+    },
+    // Create a new user
+    createNewUser({ body}, res) {
+
+    },
+    // Update an existing user
+    updateUser({ params, body}, res) {
+
+    },
+    // Delete existing user
+    removeUser({ params }, res) {
+
+    },
+    // Add a new friend
+    addFriend({ params }, res) {
+
+    },
+    // Delete an exisiting friend
+    removeFriend({ params }, res) {
+
+    }
+};
+
+module.exports = userController;
