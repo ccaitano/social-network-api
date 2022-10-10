40f1bfafa9db5b58977794aac7d65728ffc93f6a
ccaitano <cheryl.caitano@gmail.com>
2022-10-09 16:24:38 -1000

    Working on thoughts.js controller file

--- controllers/thoughts.js
+++ controllers/thoughts.js
@@ -3,31 +3,96 @@ const { Thought, User } = require('../models');
 const thoughtController = {
     // Get all thoughts
     getAllThoughts(req, res){
-        
+        Thought.find()
+            .then(thoughtData => res.json(thoughtData))
+            .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+            });
     },
     // Get a single thought by ID
-    getThoughtById({ params }, res) {
-
+    getThoughtById(req , res) {
+        Thought.findOne({ _id: req.params.thoughtId })
+            .then(thoughtData => {
+                if(thoughtData) {
+                    res.json(thoughtData);
+                } else {
+                    res.status(400).json({message: 'No Thought Associated with This ID!'});
+                }
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+            });
     },
     // Create a new thought
-    createThought({ params, body}, res) {
-
+    createThought(req, res) {
+        Thought.create(req.body)
+            .then(thoughtData => res.json(thoughtData))
+            .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+            })
     },
     // Update an existing thought by ID
-    updateThought({ params, body}, res) {
-
+    updateThought(req, res) {
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $set: req.body },
+            { runValidators: true, new: true}
+            )
+            .then(thoughtData => {
+                if(thoughtData) {
+                    res.json(thoughtData);
+                } else {
+                    res.status(400).json({message: 'No Thought Associated with This ID!'});
+                };
+            })
     },
     // Delete an existing thought by ID
-    removeThought({ params }, res) {
-
+    removeThought(req, res) {
+        Thought.findOneAndDelete({ _id: req.params.thoughtId })
+            .then(thoughtData => {
+                if(thoughtData){
+                    res.json(thoughtData);
+                } else {
+                    res.status(400).json({message: 'No Thought Associated with This ID!'});
+                };
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+            })
     },
     // Add a reaction to a thought
-    addReaction({params, body}, res) {
-
+    addReaction(req, res) {
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtID },
+            { $addToSet: { reactions: req.body } },
+            { runValidators: true, new: true }
+        )
+            .then(thoughtData => {
+                if(thoughtData) {
+                    res.json(thoughtData);
+                } else {
+                    res.status(400).json({message: 'No Thought Associated with This ID!'});
+                };
+            })
     },
     // Delete a reaction from a thought
-    removeReaction({ params }, res) {
-
+    removeReaction(req, res) {
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtID },
+            { $pull: { reactions: { reactionId: req.params.reactionID }}},
+            { runValidators: true, new: true }
+        )
+            .then(thoughtData => {
+                if(thoughtData) {
+                    res.json(thoughtData);
+                } else {
+                    res.status(400).json({message: 'No Thought Associated with This ID!'});
+                };
+            })
     }
 };
 
